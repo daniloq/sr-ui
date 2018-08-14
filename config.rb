e3d2f9b7892bd757fffc0f8e3c8841cfed23d9d1
Danilo Quilaton <daniloquilaton@gmail.com>
2018-08-13 22:46:44 -0700

    Update config

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Use relative URLs
+activate :relative_assets
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
